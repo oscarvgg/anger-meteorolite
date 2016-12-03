36e9962cff76110826bd6072adb41611a5cd6693
Oscar Vicente Gonzalez Greco <gonzalezgreco@gmail.com>
2016-12-03 22:52:59 +0100

    added the fall behaviour

---
+++ Assets/Scripts/FallBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class FallBehaviour : MonoBehaviour {
+
+	public GameObject meteor;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.MoveTowardsObject(meteor);
+	}
+
+	void MoveTowardsObject(GameObject player) {
+		var controller = player.GetComponent(typeof(PlayerController)) as PlayerController;
+		var towardsPosition = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z);
+		transform.position = Vector3.MoveTowards(
+			transform.position, 
+			towardsPosition, 
+			controller.fallSpeed * Time.deltaTime);
+	}
+
+}
