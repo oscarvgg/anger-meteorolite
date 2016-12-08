0888dc4b466fd5399eab69914d3ac384baca2fcc
Jose Vega <josevega_eci@hotmail.com>
2016-12-08 18:31:37 +0100

    Put textures and some parts of the planet, although always they go to the same play because differents play game aren’t made.

---
+++ Assets/Scripts/ChoosePart.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChoosePart : MonoBehaviour {
+
+	Ray ray;
+	RaycastHit hit;
+
+	void Update()
+	{
+		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast(ray, out hit) && Input.GetMouseButton(0))
+		{
+			if (hit.collider.name == "WHITE") 
+			{
+				SceneManager.LoadScene("PlayerController_Test");
+			}
+			if (hit.collider.name == "WHITE2") 
+			{
+				SceneManager.LoadScene("PlayerController_Test");
+			}
+			if (hit.collider.name == "BLUE1") 
+			{
+				SceneManager.LoadScene("PlayerController_Test");
+			}
+			if (hit.collider.name == "PURPLE1") 
+			{
+				SceneManager.LoadScene("PlayerController_Test");
+			}
+			if (hit.collider.name == "PURPLE2") 
+			{
+				SceneManager.LoadScene("PlayerController_Test");
+			}
+		}
+	}
+
+
+
+}
