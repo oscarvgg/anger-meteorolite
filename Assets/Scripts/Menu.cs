fd65c11546e2115068e96fcabe1efab897c329c1
Jose Vega <josevega_eci@hotmail.com>
2016-12-09 19:54:15 +0100

    Menu except settings and power up which aren’t implementated

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Menu: MonoBehaviour
+{
+	public void play(){
+		SceneManager.LoadScene ("Planets");
+	}
+}
