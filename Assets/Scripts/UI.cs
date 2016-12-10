758ef9d2a0f3f501ffa7d5bc1136bcaf305c068b
Tabinda Mumtaz <engr.tabu@gmail.com>
2016-12-10 12:31:02 +0100

    Added the main menu

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour
+{
+    //start function
+
+    public void Start()
+    {
+        SceneManager.LoadScene("----");
+    }
+    //power up function
+    public void PowerUp()
+    {
+        SceneManager.LoadScene("");
+
+    }
+    //setting function
+    public void Setting()
+
+    {
+        SceneManager.LoadScene("");
+    }
+}
+
