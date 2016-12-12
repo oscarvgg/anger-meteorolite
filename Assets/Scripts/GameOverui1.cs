075979e26cd14042da483058e6c49fd63e0f7f5f
Tabinda Mumtaz <engr.tabu@gmail.com>
2016-12-12 22:36:42 +0100

    committed gameoverui

---
+++ Assets/Scripts/GameOverui1.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour
+{
+    public string gameLevel1 = "MAINMENU";
+
+    public void Mainmenu()
+    {
+        // Application.LoadLevel(gameLevel1);
+        SceneManager.LoadScene(gameLevel1);
+    }
+    public void exit()
+    { 
+       
+        Application.Quit();
+
+    }
+  
+
+}
