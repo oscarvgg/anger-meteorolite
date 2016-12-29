1898fc7d2d9e10d740a365dfa2db570ac9a1459c
Jose Vega <JoseV@MacBook-Pro-de-Jose.local>
2016-12-29 13:23:32 +0100

    Height, weight and back button

---
+++ Assets/Scripts/Weight.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class Weight : MonoBehaviour {
+	float ValMax = 100;
+	float ValActual = 100;
+
+
+
+	public Image barra;
+
+	public Text valText;
+
+	void Start(){
+		
+	}
+	void Update(){
+		valText.text = ValActual.ToString ();
+	}
+
+	void IntroValActual(float miBarra)
+	{
+		barra.fillAmount = miBarra;
+	}
+
+	void OnCollisionEnter(Collision col){
+		
+		if (col.gameObject.tag == "Missile") {
+			Debug.Log ("Hola");
+			ValActual -= 1;
+			float vidaBarra = ValActual / ValMax;
+			IntroValActual (vidaBarra);
+
+		}
+
+	}
+}
