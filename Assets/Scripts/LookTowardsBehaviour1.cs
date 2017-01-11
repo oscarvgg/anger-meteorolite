48afb3a65b89f603238db64c71c4c0722e553950
Tabinda Mumtaz <engr.tabu@gmail.com>
2017-01-12 00:45:46 +0100

    committeddd

---
+++ Assets/Scripts/LookTowardsBehaviour1.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookTowardsBehaviour1 : MonoBehaviour {
+
+	public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		this.transform.LookAt(target.transform.position, Vector3.up);
+	}
+}
