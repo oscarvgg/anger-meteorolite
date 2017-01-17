a8d150fe58dabb2515f59aa94b1a21b0d40245d0
Jose Vega <josevega_eci@hotmail.com>
2017-01-17 20:18:46 +0100

    Skins

--- Assets/Scripts/changeskin.cs
+++ Assets/Scripts/changeskin.cs
@@ -10,24 +10,32 @@ public class changeskin : MonoBehaviour
 	public Material meteor;
 	public Texture basket;
 	public Texture orange;
+	public Texture hole;
+	public Texture sun;
+	public Texture magic;
 
 
     // Use this for initialization
     public void ChangeToSceneNext()
     {
-		if (original.GetComponent<Renderer> ().material.mainTexture==meteor.mainTexture)
-        {
-			original.GetComponent<Renderer> ().sharedMaterial.mainTexture=orange;
+		if (original.GetComponent<Renderer> ().material.mainTexture == meteor.mainTexture) {
+			original.GetComponent<Renderer> ().sharedMaterial.mainTexture = orange;
 			GameObject.Find ("TypeSkin").GetComponent<Skin> ().skin = orange;
 
-		}else if (original.GetComponent<Renderer> ().material.mainTexture==orange)
-		{
-			original.GetComponent<Renderer> ().sharedMaterial.mainTexture=basket;
+		}else if (original.GetComponent<Renderer> ().material.mainTexture == orange) {
+			original.GetComponent<Renderer> ().sharedMaterial.mainTexture = basket;
 			GameObject.Find ("TypeSkin").GetComponent<Skin> ().skin = basket;
 
-        }
-        else
-        {
+		}else if (original.GetComponent<Renderer> ().material.mainTexture == basket) {
+			original.GetComponent<Renderer> ().sharedMaterial.mainTexture = hole;
+			GameObject.Find ("TypeSkin").GetComponent<Skin> ().skin = hole;
+		}else if (original.GetComponent<Renderer> ().material.mainTexture == hole) {
+			original.GetComponent<Renderer> ().sharedMaterial.mainTexture = sun;
+			GameObject.Find ("TypeSkin").GetComponent<Skin> ().skin = sun;
+		}else if (original.GetComponent<Renderer> ().material.mainTexture == sun) {
+			original.GetComponent<Renderer> ().sharedMaterial.mainTexture = magic;
+			GameObject.Find ("TypeSkin").GetComponent<Skin> ().skin = magic;
+		}else{
 			original.GetComponent<Renderer> ().sharedMaterial.mainTexture=meteor.mainTexture;
 			GameObject.Find ("TypeSkin").GetComponent<Skin> ().skin = meteor.mainTexture;
         }
@@ -36,21 +44,26 @@ public class changeskin : MonoBehaviour
     public void ChangeToSceneBack()
     {
 
-		if (original.GetComponent<Renderer> ().material.mainTexture==meteor.mainTexture)
-		{
-			original.GetComponent<Renderer> ().sharedMaterial.mainTexture=basket;
-			GameObject.Find ("TypeSkin").GetComponent<Skin> ().skin = basket;
+		if (original.GetComponent<Renderer> ().material.mainTexture==meteor.mainTexture){
+			original.GetComponent<Renderer> ().sharedMaterial.mainTexture=magic;
+			GameObject.Find ("TypeSkin").GetComponent<Skin> ().skin = magic;
 
-		}else if (original.GetComponent<Renderer> ().material.mainTexture==orange)
-		{
+		}else if (original.GetComponent<Renderer> ().material.mainTexture==magic){
+			original.GetComponent<Renderer> ().sharedMaterial.mainTexture=sun;
+			GameObject.Find ("TypeSkin").GetComponent<Skin> ().skin = sun;
+
+		}else if (original.GetComponent<Renderer> ().material.mainTexture == sun) {
+			original.GetComponent<Renderer> ().sharedMaterial.mainTexture = hole;
+			GameObject.Find ("TypeSkin").GetComponent<Skin> ().skin = hole;
+		}else if (original.GetComponent<Renderer> ().material.mainTexture == hole) {
+			original.GetComponent<Renderer> ().sharedMaterial.mainTexture = basket;
+			GameObject.Find ("TypeSkin").GetComponent<Skin> ().skin = basket;
+		}else if (original.GetComponent<Renderer> ().material.mainTexture == basket) {
+			original.GetComponent<Renderer> ().sharedMaterial.mainTexture = orange;
+			GameObject.Find ("TypeSkin").GetComponent<Skin> ().skin = orange;
+		}else{
 			original.GetComponent<Renderer> ().sharedMaterial.mainTexture=meteor.mainTexture;
 			GameObject.Find ("TypeSkin").GetComponent<Skin> ().skin = meteor.mainTexture;
-
-		}
-		else
-		{
-			original.GetComponent<Renderer> ().sharedMaterial.mainTexture=orange;
-			GameObject.Find ("TypeSkin").GetComponent<Skin> ().skin = orange;
 		}
 
 
