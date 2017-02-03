c5bf44e9b9f80dac05b39d85ef66733384128397
Jose Vega <joseveg_eci@hotmail.com>
2017-02-03 23:59:57 +0100

    TODO

---
+++ Assets/Scripts/Opening.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Opening : MonoBehaviour {
+
+	public GameObject team, polimi;
+	// Use this for initialization
+	void Start () {
+		team.SetActive (true);
+		polimi.SetActive (false);
+		StartCoroutine (Team());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator Team(){
+		yield return new WaitForSeconds(3f);
+		team.SetActive (false);
+		polimi.SetActive (true);
+		StartCoroutine (Polimi());
+	}
+
+	IEnumerator Polimi(){
+		yield return new WaitForSeconds(3f);
+		SceneManager.LoadScene ("MAINPAGE");
+	}
+}
