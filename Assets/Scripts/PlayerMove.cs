42c4ef8e475a89c0d6572bdd2b0004167ca80d18
Jose Vega <joseveg_eci@hotmail.com>
2017-02-03 23:16:54 +0100

    Done

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -31,35 +31,21 @@ public class PlayerMove : MonoBehaviour {
 	}
 
 	void MoveUpdate(){
-		if (Application.platform == RuntimePlatform.IPhonePlayer&&falling==true) {
-			pos=Camera.main.ScreenToWorldPoint(new Vector3(Input.GetTouch(0).position.x,Input.GetTouch(0).position.y,1));
-		}else{
+		if (Application.platform == RuntimePlatform.IPhonePlayer && falling == true) {
+			pos = Camera.main.ScreenToWorldPoint (new Vector3 (Input.GetTouch (0).position.x, Input.GetTouch (0).position.y, 1));
+		} else {
 			if (falling == true) {
 				pos = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 1.5f));
 			}
 		}
+		transform.position = new Vector3 (pos.x, pos.y - 1f, pos.z + 0.1f);
 
 		if (GameObject.Find ("Settings") != null) {
 			float dis = GameObject.Find ("Settings").GetComponent<SettingGame> ().camera;
-			transform.position = new Vector3 (pos.x, pos.y -1.5f + dis, pos.z+0.1f);
-		}
-		if(posZup==0 && posZdown==0 && posXLeft==0 && posXRight==0){
-			transform.Rotate(rotation);
-		}
-		if (meteor.transform.position.z <= 46) {
-			this.transform.Translate (0, 0, posZup);
-		}
-		if (meteor.transform.position.z >= 43.8) {
-			this.transform.Translate (0, 0, posZdown);
-		}
-
-		if (meteor.transform.position.x <= 1.8) {
-			this.transform.Translate (posXRight, 0,0 );
-		}
-		if (meteor.transform.position.x >= -1.8) {
-			this.transform.Translate (posXLeft, 0,0 );
+			transform.position = new Vector3 (pos.x, pos.y - 1f + dis, pos.z + 0.1f);
 		}
 	}
+
 
 
 
@@ -88,102 +74,5 @@ public class PlayerMove : MonoBehaviour {
 		speed = 0.00000001f;
 	}
 
-	public void GoUp(){
-		rotation = Vector3.zero;
-		if (meteor.transform.position.z <= 46) {
-			
-			posZup = 1.5f*Time.deltaTime;
-		}
-	}
 
-	public void GoDown(){
-		rotation = Vector3.zero;
-		if (meteor.transform.position.z >= 43.8) {
-			
-			posZdown = -1.5f*Time.deltaTime;
-		}
-	}
-
-	public void GoRight(){
-		rotation = Vector3.zero;
-		if (meteor.transform.position.x <= 1.8) {
-			
-			posXRight = 1.5f*Time.deltaTime;
-		}
-	}
-
-	public void GoLeft(){
-		rotation = Vector3.zero;
-		if (meteor.transform.position.x >= -1.8) {
-
-			posXLeft = -1.5f*Time.deltaTime;
-		}
-	}
-
-	public void GoLeftDown(){
-		rotation = Vector3.zero;
-		if (meteor.transform.position.x >= -1.8) {
-
-			posXLeft = -1.5f*Time.deltaTime;
-		}
-
-		if (meteor.transform.position.z >= 43.8) {
-
-			posZdown = -1.5f*Time.deltaTime;
-		}
-	}
-
-	public void GoLeftUp(){
-		rotation = Vector3.zero;
-		if (meteor.transform.position.x >= -1.8) {
-
-			posXLeft = -1.5f*Time.deltaTime;
-		}
-
-		if (meteor.transform.position.z <= 46) {
-
-			posZup = 1.5f*Time.deltaTime;
-		}
-	}
-
-	public void GoRightDown(){
-		rotation = Vector3.zero;
-		if (meteor.transform.position.x <= 1.8) {
-
-			posXRight = 1.5f*Time.deltaTime;
-		}
-
-		if (meteor.transform.position.z >= 43.5) {
-
-			posZdown = -1.5f*Time.deltaTime;
-		}
-	}
-
-	public void GoRightUp(){
-		rotation = Vector3.zero;
-		if (meteor.transform.position.x <= 1.8) {
-
-			posXRight = 1.5f*Time.deltaTime;
-		}
-
-		if (meteor.transform.position.z <= 46) {
-
-			posZup = 1.5f*Time.deltaTime;
-		}
-	}
-
-
-
-
-
-	public void Anything(){
-		//rotation = new Vector3 (0, 0, Time.deltaTime * 10);
-		posZup = 0;
-		posZdown = 0;
-		posXRight=0;
-		posXLeft=0;
-		rotation = new Vector3 (Time.deltaTime * 10, Time.deltaTime * 10, Time.deltaTime * 10);
-
-
-	}
 }
